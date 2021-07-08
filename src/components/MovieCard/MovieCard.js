1b7d6510bd316728eaf66559b7a1c1d80dbeed36
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-07 20:48:04 -0600

    Remove box borders and round rating to 1 decimal

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,10 +3,11 @@ import './MovieCard.css'
 
 const MovieCard = ({title, rating, imageSrc}) => {
 
+    let newRating = rating.toFixed(1)
     return (
         <div className='movie-card'>
             <h3>{title}</h3>
-            <p>{rating}</p>
+            <p>{newRating} ⭐️</p>
             <img src={imageSrc} alt={`${title} movie poster`}/>
         </div>
     )
