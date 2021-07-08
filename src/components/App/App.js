a7ef93a8257d3d75b50f310cdf5b2d194f01116c
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-08 16:50:57 -0600

    Test movie details rendering without conditional

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Movies from '../Movies/Movies';
+import Details from '../Details/Details';
 import movieData from '../../movieData';
 import './App.css';
 
@@ -8,7 +9,7 @@ class App extends Component {
     super();
     this.state = {
       movies: movieData.movies,
-      selectedMovie:[]
+      selectedMovie: {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }
     }
     this.displayMovie = this.displayMovie.bind(this);
   }
@@ -24,10 +25,15 @@ class App extends Component {
         <nav>
           <h1>Rancid Tomatillos</h1>
         </nav>
-        <Movies 
+        {/* {Object.keys(this.state.selectedMovie).length && */}
+          <Details 
+            selectedMovie={this.state.selectedMovie}
+          />
+        {/* } */}
+        {/* <Movies 
           movies={this.state.movies} 
           displayMovie={this.displayMovie}
-        />
+        /> */}
       </React.Fragment>
     );
   }
