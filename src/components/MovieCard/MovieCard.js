43e2db55c93c06f647fdb844f1a5cca3db873523
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-12 19:36:52 -0600

    Update path to movie details

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,7 +5,7 @@ import './MovieCard.css'
 const MovieCard = ({id, title, rating, imageSrc, displayMovie}) => {
 
     return (
-        <Link to={`/movies/${id}`}>
+        <Link to={`/movie/${id}`}>
             <div className='movie-card' onClick={() => displayMovie(id)}>
                 <img src={imageSrc} alt={`${title} movie poster`}/>
                 <div className='title-rating'>
