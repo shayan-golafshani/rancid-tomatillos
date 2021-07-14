96eaa93a48475a6d1af02088d6c326ea9edee1d1
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-13 18:16:48 -0600

    Update movie_dashboard.js user flow tests

--- cypress/integration/movies_dashboard_spec.js
+++ cypress/integration/movies_dashboard_spec.js
@@ -7,6 +7,33 @@ describe ('User Movie Selection flows', () => {
     it('Navbar should contain website title', () => {
        cy.get('h1').contains('Rancid Tomatillos')
     })
+
+    it('Should be able to click first movie card, should update url to matching path and display details', () => {
+
+        cy.url().then(url => {
+        
+            cy.get('.movie-card:first').click()
+
+            cy.url().should('not.eq', url);
+            cy.get('button').should('contain', 'Go Back')
+          });
+
+
+        // cy.get('.movie-card:first').click()
+        
+        // .click()
+        // cy.url().should('include', 'localhost')
+//cy.get('li:first').click({
+    })
+
+   // it('Should be able to click on a movie-card and render those movie details', () => {
+    //keep working on this test!
+
+        //cy.get('.movie-card').should('have.class', 'hover')
+
+        //cy.get('.title-rating').should('be.hidden').invoke('show').click()
+        //cy.get('#message').should('contain', 'the button was clicked')
+    //})
 })
 
 // describe('Feedback Loop login flows', () => {
