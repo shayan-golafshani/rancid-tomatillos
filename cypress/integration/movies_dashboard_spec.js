8aeac21e1c2994876c34544e3ca72c2c68e6b6cf
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-14 16:26:03 -0600

    Update cypress test with an intercept and fixtures

--- cypress/integration/movies_dashboard_spec.js
+++ cypress/integration/movies_dashboard_spec.js
@@ -1,19 +1,31 @@
 describe ('User Movie Selection flows', () => {
 
     beforeEach(() => {
-        cy.visit('http://localhost:3000/')
+        cy.fixture('movieData').then((movies) => {
+            const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2';
+            cy.intercept('GET', `${baseURL}//movies`, {
+                statusCode: 201,
+                  body: movies,
+              });
+            })
+            cy.visit('http://localhost:3000/')
     })
 
     it('Navbar should contain website title', () => {
        cy.get('h1').contains('Rancid Tomatillos')
     })
 
+    it('Should be able to display movie cards on load', () => {
+
+    })
+
     it('Should be able to click first movie card, should update url to matching path and display details', () => {
 
         cy.url().then(url => {
-        
             cy.get('.movie-card:first').click()
+            //cy.url().should('include', 'http://localhost:3000/694919')
 
+            cy.url().should('eq', 'http://localhost:3000/694919')
             cy.url().should('not.eq', url);
             cy.get('button').should('contain', 'Go Back')
           });
