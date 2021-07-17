07dd10e09dd696f89ebc4caf8fb07d225b534d13
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-17 14:17:43 -0600

    Edit classNames and update styling for errors

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link, Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Error.css'
 import '../Resources/tomato1.png'
 
 const Error = (props) => {
 
     return (
-        <div className='error-message'>         
+        <div className='error'>         
             <Link to ='/'>
                 {
                     //put an error status code up in here!
@@ -14,9 +14,9 @@ const Error = (props) => {
                 {
                     //<h2>{props.type}</h2>
                 }
-                <h3 className='error-message'>Something went wrong. Click here to be redirected</h3>
+                <h3 className='error-message'>Something went wrong. Click here to be redirected.</h3>
             </Link>
-            <img id='error-gif' src='https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif' alt='disappointed animated tomato'/>
+            <img className='error-gif' id='error-gif' src='https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif' alt='disappointed animated tomato'/>
         </div>
     )
 
