faebbf9a544f1d8992999d60605e52638464bab7
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-19 17:35:47 -0600

    Clean up apiCalls file and add catches

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,24 +1,22 @@
 const getAllMovies = () => {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
         .then(response => response.json())
+        .catch(err => console.error(err))
 }
 
 const getMovieDetails = (id) => {
     return fetch (`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
         .then(response => response.json())
+        .catch(err => console.error(err))
 }
 
 const getMovieTrailer = (id) => {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`) 
         .then(response => response.json())
+        .catch(err => console.error(err))
 }
 
-const getAllMovieDetails =(id) => {
-    return Promise.all([getMovieDetails(id), getMovieTrailer(id)])
-        //  .then(response => {
-        //     console.log(response) 
-        //     return response.json()})
-}
+const getAllMovieDetails =(id) => Promise.all([getMovieDetails(id), getMovieTrailer(id)])
 
 
 export { getAllMovies, getAllMovieDetails}
