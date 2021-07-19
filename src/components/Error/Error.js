c0966203c063ad5d32afc2d6cf1ca1bfc7b33618
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-19 14:59:28 -0600

    Remove code comments

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -8,12 +8,6 @@ const Error = (props) => {
     return (
         <div className='error'>         
             <Link to ='/'>
-                {
-                    //put an error status code up in here!
-                }
-                {
-                    //<h2>{props.type}</h2>
-                }
                 <h3 className='error-message'>Something went wrong. Click here to be redirected.</h3>
             </Link>
             <img className='error-gif' id='error-gif' src='https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif' alt='disappointed animated tomato'/>
--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -6,14 +6,8 @@ const Loading = () => {
 
     return (
         <div className='loading'>
-        <h2 className='loading-message'>Loading ...</h2>
+            <h2 className='loading-message'>Loading ...</h2>
             <img className='tomato-image' id='tomato-1' src='https://toppng.com/public/uploads/thumbnail/green-tomato-cartoon-115496727097vw6f4by8k.png' alt='happy larry tomato'/>      
-        {//<div class="tenor-gif-embed" data-postid="21977363" data-share-method="host" data-width="100%" data-aspect-ratio="1.0"><a href="https://tenor.com/view/tomato-spin-gif-21977363">Tomato Spin GIF</a> from <a href="https://tenor.com/search/tomatospin-gifs">Tomatospin GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
-        }
-        {/* <img id='loading-gif' src='https://media.giphy.com/media/eg7NbI9e4UN9e/giphy-downsized.gif' alt='spinning tomatoes on a platter'/> */}
-        {//<img src='' />
-        //<div class="tenor-gif-embed" data-postid="13072790" data-share-method="host" data-width="100%" data-aspect-ratio="1.45"><a href="https://tenor.com/view/tomato-dance-gif-13072790">Tomato Dance GIF</a> from <a href="https://tenor.com/search/tomato-gifs">Tomato GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
-        }
         </div>
     )
 
