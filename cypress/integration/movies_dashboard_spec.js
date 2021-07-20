c0d1741bd8ec7cee110b20b3c165e475aebb9c4d
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-20 05:16:16 -0600

    Add more test coverage to dashboard

--- cypress/integration/movies_dashboard_spec.js
+++ cypress/integration/movies_dashboard_spec.js
@@ -9,12 +9,16 @@ describe ('User Movie Selection flows', () => {
               });
             })
             cy.visit('http://localhost:3000/')
-            cy.wait(1000)
+            cy.wait(500)
     })
 
     it('Navbar should contain website title', () => {
        cy.get('h1').contains('Rancid Tomatillos')
     })
+
+    it('Should be able to visit http://localhost:3000 and see the first movie poster', () => {
+      cy.get('img:first').should('be.visible')
+    });
 
     it('Should contain a searchbar, that can be typed in', () => {
         cy.get('input').type('Mul')
@@ -22,13 +26,13 @@ describe ('User Movie Selection flows', () => {
         cy.url().should('eq', 'http://localhost:3000/337401')
     })
 
-    it('Should be able to visit http://localhost:3000 and see the first movie poster', () => {
-      cy.get('img:first').should('be.visible')
-  });
 
     it('Should be able to display movie cards on load', () => {
-        //cy.get('#id')
-    })
+        cy.get('img').each($el => {
+            cy.wrap($el).should('be.visible')
+            cy.wrap($el).should('have.attr', 'alt')
+          })
+        })
 
 
     it('Should be able to click first movie card, should update url to matching path and display details', () => {
