9214732ed4bae42d199b935f6174ca99da0fe1af
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-19 18:20:44 -0600

    Remove commented out cypress

--- cypress/integration/movies_dashboard_spec.js
+++ cypress/integration/movies_dashboard_spec.js
@@ -16,14 +16,14 @@ describe ('User Movie Selection flows', () => {
     })
 
     it('Should be able to display movie cards on load', () => {
-        //cy.get('#id')
+        
     })
 
     it('Should be able to click first movie card, should update url to matching path and display details', () => {
 
         cy.url().then(url => {
             cy.get('.movie-card:first').click()
-            //cy.url().should('include', 'http://localhost:3000/694919')
+            
 
             cy.url().should('eq', 'http://localhost:3000/694919')
             cy.url().should('not.eq', url);
@@ -31,25 +31,8 @@ describe ('User Movie Selection flows', () => {
           });
 
 
-        // cy.get('.movie-card:first').click()
-        
-        // .click()
-        // cy.url().should('include', 'localhost')
-//cy.get('li:first').click({
     })
 
-   // it('Should be able to click on a movie-card and render those movie details', () => {
-    //keep working on this test!
-
-        //cy.get('.movie-card').should('have.class', 'hover')
-
-        //cy.get('.title-rating').should('be.hidden').invoke('show').click()
-        //cy.get('#message').should('contain', 'the button was clicked')
-    //})
+  
 })
 
-// describe('Feedback Loop login flows', () => {
-//     it('Should confirm that true is equal to true', () => {
-//       expect(true).to.equal(true)
-//     });
-//   });
