b0fc54eb07e6df47158b2e28e4a3a20a44fcb49f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-20 05:04:41 -0600

    Add happy path testing for filter bar

--- cypress/integration/movies_dashboard_spec.js
+++ cypress/integration/movies_dashboard_spec.js
@@ -9,30 +9,35 @@ describe ('User Movie Selection flows', () => {
               });
             })
             cy.visit('http://localhost:3000/')
+            cy.wait(1000)
     })
 
     it('Navbar should contain website title', () => {
        cy.get('h1').contains('Rancid Tomatillos')
     })
 
-    it('Should be able to display movie cards on load', () => {
-        
+    it('Should contain a searchbar, that can be typed in', () => {
+        cy.get('input').type('Mul')
+        cy.get('img:first').click()
+        cy.url().should('eq', 'http://localhost:3000/337401')
     })
+
+    it('Should be able to visit http://localhost:3000 and see the first movie poster', () => {
+      cy.get('img:first').should('be.visible')
+  });
+
+    it('Should be able to display movie cards on load', () => {
+        //cy.get('#id')
+    })
+
 
     it('Should be able to click first movie card, should update url to matching path and display details', () => {
-
         cy.url().then(url => {
             cy.get('.movie-card:first').click()
-            
-
             cy.url().should('eq', 'http://localhost:3000/694919')
             cy.url().should('not.eq', url);
-            cy.get('button').should('contain', 'Go Back')
           });
-
-
     })
 
-  
 })
 
